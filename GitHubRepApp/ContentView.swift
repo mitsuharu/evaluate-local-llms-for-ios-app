93d12354b5451b5312ec3c28e87b739674686e37
Mitsuharu Emoto <mthr1982@gmail.com>
2026-06-13 21:04:23 +0900

    add xcode-project

---
+++ GitHubRepApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubRepApp
+//
+//  Created by Mitsuharu Emoto on 2026/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubRepApp/GitHubRepAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepAppApp.swift
+//  GitHubRepApp
+//
+//  Created by Mitsuharu Emoto on 2026/06/13.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
